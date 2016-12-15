0bd45b92ccdc7a5c5da805c27c17926c99b71958
Moises Meirelles <mshmeirelles@gmail.com>
2016-12-15 16:19:15 -0200

    Thank you page

--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -47,8 +47,8 @@ class SubscribePostValid(TestCase):
         self.resp = self.client.post('/inscricao/', data)
 
     def test_post(self):
-        """Valid POST should redirect to /inscricao/"""
-        self.assertEqual(302, self.resp.status_code)
+        """Valid POST should redirect to /inscricao/1/"""
+        self.assertRedirects(self.resp, '/inscricao/1/')
 
     def test_send_subscribe_email(self):
         self.assertEqual(1, len(mail.outbox))
@@ -78,12 +78,3 @@ class SubscribePostInvalid(TestCase):
 
     def test_dont_save_subscription(self):
         self.assertFalse(Subscription.objects.exists())
-
-
-class SubscribeSuccessMessage(TestCase):
-    def test_message(self):
-        data = dict(name='Moises Meirelles', cpf='05270177951',
-                    email='mshmeirelles@gmail.com', phone='46999231319')
-
-        response = self.client.post('/inscricao/', data, follow=True)
-        self.assertContains(response, 'Inscrição realizada com sucesso')
--- eventex/urls.py
+++ eventex/urls.py
@@ -1,26 +1,12 @@
-"""eventex URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url
 from django.contrib import admin
 
 from eventex.core.views import home
-from eventex.subscriptions.views import subscribe
+from eventex.subscriptions.views import subscribe, detail
 
 urlpatterns = [
     url(r'^$', home),
     url(r'^inscricao/$', subscribe),
+    url(r'^inscricao/(\d+)/$', detail),
     url(r'^admin/', admin.site.urls),
 ]
