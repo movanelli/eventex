d8e84c171f96f884addf01a68a68308082bfc46b
Moises Meirelles <mshmeirelles@gmail.com>
2017-01-02 17:01:52 -0200

    Email and Phone is optional

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -20,10 +20,25 @@ class SubscriptionFormTest(TestCase):
         form = self.make_validated_form(cpf='0527')
         self.assertFormErrorCode(form, 'cpf', 'length')
 
-    def test_name_is_capitalized(self):
+    def test_name_must_be_capitalized(self):
         """Name must be capitalized"""
         form = self.make_validated_form(name='MOISES meirelles')
         self.assertEqual('Moises Meirelles', form.cleaned_data['name'])
+
+    def test_email_is_optional(self):
+        """Email is optional"""
+        form = self.make_validated_form(email='')
+        self.assertFalse(form.errors)
+
+    def test_phone_is_optional(self):
+        """Phone is optional"""
+        form = self.make_validated_form(phone='')
+        self.assertFalse(form.errors)
+
+    def test_must_inform_email_or_phone(self):
+        """Email and Phone are optional, but one must be informed"""
+        form = self.make_validated_form(email='', phone='')
+        self.assertListEqual(['__all__'], list(form.errors))
 
     def assertFormErrorCode(self, form, field, code):
         errors = form.errors.as_data()
