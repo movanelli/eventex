90a4c3107d8ef3202cbbccd609f3d368e1ca1db3
Moises Meirelles <mshmeirelles@gmail.com>
2017-01-09 20:51:50 -0200

    Talk list

--- eventex/core/tests/test_view_talk_list.py
+++ eventex/core/tests/test_view_talk_list.py
@@ -1,7 +1,27 @@
+from django.shortcuts import resolve_url as r
 from django.test import TestCase
 
 
 class TalkListGet(TestCase):
+    def setUp(self):
+        self.resp = self.client.get(r('talk_list'))
+
     def test_get(self):
-        response = self.client.get('/palestras/')
-        self.assertEqual(200, response.status_code)
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        self.assertTemplateUsed(self.resp, 'core/talk_list.html')
+
+    def test_html(self):
+        contents = [
+            (2, 'Título da Palestra'),
+            (1, '10:00'),
+            (1, '13:00'),
+            (2, '/palestrantes/moises-meirelles'),
+            (2, 'Moises Meirelles'),
+            (2, 'Descrição da Palestra.'),
+        ]
+
+        for count, expected in contents:
+            with self.subTest():
+                self.assertContains(self.resp, expected, count)
--- eventex/urls.py
+++ eventex/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^$', home, name='home'),
     url(r'^inscricao/', include('eventex.subscriptions.urls',
                                 namespace='subscriptions')),
-    url(r'^palestras/$', talk_list),
+    url(r'^palestras/$', talk_list, name='talk_list'),
     url(r'^palestrantes/(?P<slug>[\w-]+)/$', speaker_detail, name='speaker_detail'),
     url(r'^admin/', include(admin.site.urls)),
 ]
